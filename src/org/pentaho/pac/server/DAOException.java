6d1628e4d640cc43ebe09964c94075346e81e8f3
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-02 21:27:02 +0000

    flesh out inherited ctors
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@22778 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/server/DAOException.java
+++ src/org/pentaho/pac/server/DAOException.java
@@ -5,14 +5,20 @@ import org.pentaho.pac.client.CheckedException;
 
 public class DAOException extends CheckedException {
   
-  public DAOException(String msg) {
-    super(msg);
+
+  public DAOException() {
+    super();
   }
-  
+
+  public DAOException(String message) {
+    super(message);
+  }
+
+  public DAOException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
   public DAOException(Throwable cause) {
     super(cause);
-  }
-  
-  public DAOException() {
-  }
+  }    
 }
