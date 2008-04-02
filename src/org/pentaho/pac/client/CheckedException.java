cc1fa4159afbb4c275dbbbcf04372b62a3eaf433
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-02 21:25:43 +0000

    implement serializable
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@22775 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/CheckedException.java
+++ src/org/pentaho/pac/client/CheckedException.java
@@ -1,7 +1,10 @@
 package org.pentaho.pac.client;
 
-public class CheckedException extends Exception {
+import java.io.Serializable;
 
+public class CheckedException extends Exception implements Serializable {
+
+  private String msg = null;
   /**
    * 
    */
@@ -13,13 +16,19 @@ public class CheckedException extends Exception {
 
   public CheckedException(String message) {
     super(message);
+    msg = message;
   }
 
   public CheckedException(String message, Throwable cause) {
     super(message, cause);
+    msg = message;
   }
 
   public CheckedException(Throwable cause) {
     super(cause);
+  }    
+  
+  public String getMessage() {
+    return msg;
   }
 }
