e94a4f1c59b1eb2e99045db7d62fc3191aa84c52
nbaker <nbaker@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-09 12:16:28 +0000

    Initial Build files, need to be modified
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23160 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ build-res/stop.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM ***************************************
+REM   BATCH SCRIPT TO STOP MGMG CONSOLE
+REM ***************************************
+
+
+call java -jar C:\EclipseWorkspace\pentaho-administration-console\dist\pentaho-admin-console\pentaho-admin-console\pentaho-adminstration-console.jar STOP
