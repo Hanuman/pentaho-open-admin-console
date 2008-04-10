3a942a5b06c17187c9f391a811471dcd93051cd5
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-10 18:17:43 +0000

    generate the .java, fix the .properties.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23301 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/i18n/PacLocalizedMessages.java
+++ src/org/pentaho/pac/client/i18n/PacLocalizedMessages.java
@@ -1,82 +1,609 @@
 package org.pentaho.pac.client.i18n;
 
-import com.google.gwt.i18n.client.Messages;
 
-public interface PacLocalizedMessages extends Messages {
-  String usersAndRoles();
-  String dataSources();
-  String services();
-  String scheduler();
-  String ok();
-  String cancel();
-  String yes();
-  String no();
-  String administration();
-  String home();
-  String test();
-  String security();
-  String securityRefreshError(String cause);
-  String invalidTabIndex(int index);
-  String addRole();
-  String invalidRoleName();
-  String insufficientPrivileges();
-  String roleAlreadyExists();
-  String roleName();
-  String description();
-  String update();
-  String confirmRoleDeletionMsg();
-  String deleteRoles();
-  String assignedUsers();
-  String roleDoesNotExist(String roleName);
-  String updateRole();
-  String cantAssignNonexistingUserToRole(String userName);
-  String userListFilter();
-  String roles();
-  String refreshSolutionRepository();
-  String removeStaleContent();
-  String purgeMondrianDataCache();
-  String purgeMondrianSchemaCache();
-  String scheduleDailyRepositoryCleaning();
-  String restoreDefaultFilePermissions();
-  String refreshSystemSettings();
-  String executeGlobalActions();
-  String refreshReportingMetadata();
-  String addUser();
-  String invalidUserName();
-  String passwordConfirmationFailed();
-  String userAlreadyExist();
-  String userName();
-  String password();
-  String passwordConfirmation();
-  String deleteUsers();
-  String confirmUserDeletionMsg();
-  String roleListFilter();
-  String assignedRoles();
-  String userDoesNotExist(String userName);
-  String updateUser();
-  String jndiName();
-  String maxActiveDbConnections();
-  String jdbcDriverClass();
-  String numIdleDbConnnections();
-  String dbUserName();
-  String dbPassword();
-  String dbUrl();
-  String dbValidationQuery();
-  String dbWaitTime();
-  String errorLoadingDataSources();
-  String dataSourcesRefreshError(String cause);
-  String deleteDataSources();
-  String confirmDataSourceDeletionMsg();
-  String errorDeletingDataSource();
-  String updateDataSource();
-  String invalidConnectionName();
-  String missingDbUrl();
-  String missingDbDriver();
-  String missingDbUserName();
-  String errorUpdatingDataSource();
-  String testConnection();
-  String connectionTestSuccessful();
-  String addDataSource();
-  String errorCreatingDataSource();
+/**
+ * Interface to represent the messages contained in resource  bundle:
+ * 	C:/projects/pentaho1.6/pentaho-open-admin-console/src/org/pentaho/pac/client/i18n/PacLocalizedMessages.properties'.
+ */
+public interface PacLocalizedMessages extends com.google.gwt.i18n.client.Messages {
   
+  /**
+   * Translated "Validation Query:".
+   * 
+   * @return translated "Validation Query:"
+   * @gwt.key dbValidationQuery
+   */
+  String dbValidationQuery();
+
+  /**
+   * Translated "Refresh System Settings".
+   * 
+   * @return translated "Refresh System Settings"
+   * @gwt.key refreshSystemSettings
+   */
+  String refreshSystemSettings();
+
+  /**
+   * Translated "Delete Data Sources".
+   * 
+   * @return translated "Delete Data Sources"
+   * @gwt.key deleteDataSources
+   */
+  String deleteDataSources();
+
+  /**
+   * Translated "OK".
+   * 
+   * @return translated "OK"
+   * @gwt.key ok
+   */
+  String ok();
+
+  /**
+   * Translated "Add User".
+   * 
+   * @return translated "Add User"
+   * @gwt.key addUser
+   */
+  String addUser();
+
+  /**
+   * Translated "Are your sure you want to delete the selected roles?".
+   * 
+   * @return translated "Are your sure you want to delete the selected roles?"
+   * @gwt.key confirmRoleDeletionMsg
+   */
+  String confirmRoleDeletionMsg();
+
+  /**
+   * Translated "Role List Filter:".
+   * 
+   * @return translated "Role List Filter:"
+   * @gwt.key roleListFilter
+   */
+  String roleListFilter();
+
+  /**
+   * Translated "Schedule Daily Repository Cleaning".
+   * 
+   * @return translated "Schedule Daily Repository Cleaning"
+   * @gwt.key scheduleDailyRepositoryCleaning
+   */
+  String scheduleDailyRepositoryCleaning();
+
+  /**
+   * Translated ""Error Deleting Data Source"".
+   * 
+   * @return translated ""Error Deleting Data Source""
+   * @gwt.key errorDeletingDataSource
+   */
+  String errorDeletingDataSource();
+
+  /**
+   * Translated "Restore Default File Permissions".
+   * 
+   * @return translated "Restore Default File Permissions"
+   * @gwt.key restoreDefaultFilePermissions
+   */
+  String restoreDefaultFilePermissions();
+
+  /**
+   * Translated "User Name:".
+   * 
+   * @return translated "User Name:"
+   * @gwt.key dbUserName
+   */
+  String dbUserName();
+
+  /**
+   * Translated "Home".
+   * 
+   * @return translated "Home"
+   * @gwt.key home
+   */
+  String home();
+
+  /**
+   * Translated "Error Creating Data Source".
+   * 
+   * @return translated "Error Creating Data Source"
+   * @gwt.key errorCreatingDataSource
+   */
+  String errorCreatingDataSource();
+
+  /**
+   * Translated "Invalid tab index: {0}".
+   * 
+   * @return translated "Invalid tab index: {0}"
+   * @gwt.key invalidTabIndex
+   */
+  String invalidTabIndex(String arg0);
+
+  /**
+   * Translated "Role Name".
+   * 
+   * @return translated "Role Name"
+   * @gwt.key roleName
+   */
+  String roleName();
+
+  /**
+   * Translated "Delete Users".
+   * 
+   * @return translated "Delete Users"
+   * @gwt.key deleteUsers
+   */
+  String deleteUsers();
+
+  /**
+   * Translated "User List Filter:".
+   * 
+   * @return translated "User List Filter:"
+   * @gwt.key userListFilter
+   */
+  String userListFilter();
+
+  /**
+   * Translated "User already exists.".
+   * 
+   * @return translated "User already exists."
+   * @gwt.key userAlreadyExist
+   */
+  String userAlreadyExist();
+
+  /**
+   * Translated "Security".
+   * 
+   * @return translated "Security"
+   * @gwt.key security
+   */
+  String security();
+
+  /**
+   * Translated "URL:".
+   * 
+   * @return translated "URL:"
+   * @gwt.key dbUrl
+   */
+  String dbUrl();
+
+  /**
+   * Translated "Wait:".
+   * 
+   * @return translated "Wait:"
+   * @gwt.key dbWaitTime
+   */
+  String dbWaitTime();
+
+  /**
+   * Translated "Password Confirmation".
+   * 
+   * @return translated "Password Confirmation"
+   * @gwt.key passwordConfirmation
+   */
+  String passwordConfirmation();
+
+  /**
+   * Translated "Cancel".
+   * 
+   * @return translated "Cancel"
+   * @gwt.key cancel
+   */
+  String cancel();
+
+  /**
+   * Translated "Test".
+   * 
+   * @return translated "Test"
+   * @gwt.key test
+   */
+  String test();
+
+  /**
+   * Translated "Password does not match password confirmation.".
+   * 
+   * @return translated "Password does not match password confirmation."
+   * @gwt.key passwordConfirmationFailed
+   */
+  String passwordConfirmationFailed();
+
+  /**
+   * Translated "User Name".
+   * 
+   * @return translated "User Name"
+   * @gwt.key userName
+   */
+  String userName();
+
+  /**
+   * Translated "No".
+   * 
+   * @return translated "No"
+   * @gwt.key no
+   */
+  String no();
+
+  /**
+   * Translated "Missing user name.".
+   * 
+   * @return translated "Missing user name."
+   * @gwt.key missingDbUserName
+   */
+  String missingDbUserName();
+
+  /**
+   * Translated "User does not exist: {0}".
+   * 
+   * @return translated "User does not exist: {0}"
+   * @gwt.key userDoesNotExist
+   */
+  String userDoesNotExist(String arg0);
+
+  /**
+   * Translated "Administration".
+   * 
+   * @return translated "Administration"
+   * @gwt.key administration
+   */
+  String administration();
+
+  /**
+   * Translated "Roles".
+   * 
+   * @return translated "Roles"
+   * @gwt.key roles
+   */
+  String roles();
+
+  /**
+   * Translated "Execute Global Actions".
+   * 
+   * @return translated "Execute Global Actions"
+   * @gwt.key executeGlobalActions
+   */
+  String executeGlobalActions();
+
+  /**
+   * Translated "#Idle Conn:".
+   * 
+   * @return translated "#Idle Conn:"
+   * @gwt.key numIdleDbConnnections
+   */
+  String numIdleDbConnnections();
+
+  /**
+   * Translated "Connection Test Successful.".
+   * 
+   * @return translated "Connection Test Successful."
+   * @gwt.key connectionTestSuccessful
+   */
+  String connectionTestSuccessful();
+
+  /**
+   * Translated "Invalid connection name.".
+   * 
+   * @return translated "Invalid connection name."
+   * @gwt.key invalidConnectionName
+   */
+  String invalidConnectionName();
+
+  /**
+   * Translated "Are your sure you want to delete the selected users?".
+   * 
+   * @return translated "Are your sure you want to delete the selected users?"
+   * @gwt.key confirmUserDeletionMsg
+   */
+  String confirmUserDeletionMsg();
+
+  /**
+   * Translated "Update User".
+   * 
+   * @return translated "Update User"
+   * @gwt.key updateUser
+   */
+  String updateUser();
+
+  /**
+   * Translated "Purge Mondrian Schema Cache".
+   * 
+   * @return translated "Purge Mondrian Schema Cache"
+   * @gwt.key purgeMondrianSchemaCache
+   */
+  String purgeMondrianSchemaCache();
+
+  /**
+   * Translated "Error Updating Data Source".
+   * 
+   * @return translated "Error Updating Data Source"
+   * @gwt.key errorUpdatingDataSource
+   */
+  String errorUpdatingDataSource();
+
+  /**
+   * Translated "JNDI Name:".
+   * 
+   * @return translated "JNDI Name:"
+   * @gwt.key jndiName
+   */
+  String jndiName();
+
+  /**
+   * Translated "Update".
+   * 
+   * @return translated "Update"
+   * @gwt.key update
+   */
+  String update();
+
+  /**
+   * Translated "Add Role".
+   * 
+   * @return translated "Add Role"
+   * @gwt.key addRole
+   */
+  String addRole();
+
+  /**
+   * Translated "Assigned Users".
+   * 
+   * @return translated "Assigned Users"
+   * @gwt.key assignedUsers
+   */
+  String assignedUsers();
+
+  /**
+   * Translated "Missing database URL.".
+   * 
+   * @return translated "Missing database URL."
+   * @gwt.key missingDbUrl
+   */
+  String missingDbUrl();
+
+  /**
+   * Translated "Add Data Source".
+   * 
+   * @return translated "Add Data Source"
+   * @gwt.key addDataSource
+   */
+  String addDataSource();
+
+  /**
+   * Translated "Refresh Reporting Metadata".
+   * 
+   * @return translated "Refresh Reporting Metadata"
+   * @gwt.key refreshReportingMetadata
+   */
+  String refreshReportingMetadata();
+
+  /**
+   * Translated "Invalid user name.".
+   * 
+   * @return translated "Invalid user name."
+   * @gwt.key invalidUserName
+   */
+  String invalidUserName();
+
+  /**
+   * Translated "Password:".
+   * 
+   * @return translated "Password:"
+   * @gwt.key dbPassword
+   */
+  String dbPassword();
+
+  /**
+   * Translated "Delete Roles".
+   * 
+   * @return translated "Delete Roles"
+   * @gwt.key deleteRoles
+   */
+  String deleteRoles();
+
+  /**
+   * Translated ""Are your sure you want to delete the selected data sources?"".
+   * 
+   * @return translated ""Are your sure you want to delete the selected data sources?""
+   * @gwt.key confirmDataSourceDeletionMsg
+   */
+  String confirmDataSourceDeletionMsg();
+
+  /**
+   * Translated "Yes".
+   * 
+   * @return translated "Yes"
+   * @gwt.key yes
+   */
+  String yes();
+
+  /**
+   * Translated "Unable to refresh data sources list: {0}".
+   * 
+   * @return translated "Unable to refresh data sources list: {0}"
+   * @gwt.key dataSourcesRefreshError
+   */
+  String dataSourcesRefreshError(String arg0);
+
+  /**
+   * Translated "Remove Stale Conent".
+   * 
+   * @return translated "Remove Stale Conent"
+   * @gwt.key removeStaleContent
+   */
+  String removeStaleContent();
+
+  /**
+   * Translated "Password".
+   * 
+   * @return translated "Password"
+   * @gwt.key password
+   */
+  String password();
+
+  /**
+   * Translated "Update Data Source".
+   * 
+   * @return translated "Update Data Source"
+   * @gwt.key updateDataSource
+   */
+  String updateDataSource();
+
+  /**
+   * Translated "Can not assign non-existing user to role: {0}".
+   * 
+   * @return translated "Can not assign non-existing user to role: {0}"
+   * @gwt.key cantAssignNonexistingUserToRole
+   */
+  String cantAssignNonexistingUserToRole(String arg0);
+
+  /**
+   * Translated "Invalid role name.;".
+   * 
+   * @return translated "Invalid role name.;"
+   * @gwt.key invalidRoleName
+   */
+  String invalidRoleName();
+
+  /**
+   * Translated "Data Sources".
+   * 
+   * @return translated "Data Sources"
+   * @gwt.key dataSources
+   */
+  String dataSources();
+
+  /**
+   * Translated "Scheduler".
+   * 
+   * @return translated "Scheduler"
+   * @gwt.key scheduler
+   */
+  String scheduler();
+
+  /**
+   * Translated "Refresh Solution Repository".
+   * 
+   * @return translated "Refresh Solution Repository"
+   * @gwt.key refreshSolutionRepository
+   */
+  String refreshSolutionRepository();
+
+  /**
+   * Translated "Purge Mondrian Data Cache".
+   * 
+   * @return translated "Purge Mondrian Data Cache"
+   * @gwt.key purgeMondrianDataCache
+   */
+  String purgeMondrianDataCache();
+
+  /**
+   * Translated "Driver Class:".
+   * 
+   * @return translated "Driver Class:"
+   * @gwt.key jdbcDriverClass
+   */
+  String jdbcDriverClass();
+
+  /**
+   * Translated "Insufficient privileges.".
+   * 
+   * @return translated "Insufficient privileges."
+   * @gwt.key insufficientPrivileges
+   */
+  String insufficientPrivileges();
+
+  /**
+   * Translated "Role does not exist: {0} ".
+   * 
+   * @return translated "Role does not exist: {0} "
+   * @gwt.key roleDoesNotExist
+   */
+  String roleDoesNotExist(String arg0);
+
+  /**
+   * Translated "Error Loading Data Sources".
+   * 
+   * @return translated "Error Loading Data Sources"
+   * @gwt.key errorLoadingDataSources
+   */
+  String errorLoadingDataSources();
+
+  /**
+   * Translated "Max Active Conn:".
+   * 
+   * @return translated "Max Active Conn:"
+   * @gwt.key maxActiveDbConnections
+   */
+  String maxActiveDbConnections();
+
+  /**
+   * Translated "Unable to refresh security information: {0}".
+   * 
+   * @return translated "Unable to refresh security information: {0}"
+   * @gwt.key securityRefreshError
+   */
+  String securityRefreshError(String arg0);
+
+  /**
+   * Translated "Users & Roles".
+   * 
+   * @return translated "Users & Roles"
+   * @gwt.key usersAndRoles
+   */
+  String usersAndRoles();
+
+  /**
+   * Translated "Test Connection".
+   * 
+   * @return translated "Test Connection"
+   * @gwt.key testConnection
+   */
+  String testConnection();
+
+  /**
+   * Translated "Description".
+   * 
+   * @return translated "Description"
+   * @gwt.key description
+   */
+  String description();
+
+  /**
+   * Translated "Update Role".
+   * 
+   * @return translated "Update Role"
+   * @gwt.key updateRole
+   */
+  String updateRole();
+
+  /**
+   * Translated ""Assigned Roles"".
+   * 
+   * @return translated ""Assigned Roles""
+   * @gwt.key assignedRoles
+   */
+  String assignedRoles();
+
+  /**
+   * Translated "Missing database driver class.".
+   * 
+   * @return translated "Missing database driver class."
+   * @gwt.key missingDbDriver
+   */
+  String missingDbDriver();
+
+  /**
+   * Translated "Role already exists.".
+   * 
+   * @return translated "Role already exists."
+   * @gwt.key roleAlreadyExists
+   */
+  String roleAlreadyExists();
+
+  /**
+   * Translated "Services".
+   * 
+   * @return translated "Services"
+   * @gwt.key services
+   */
+  String services();
 }
