d9dde28c5f1ebca1b11ca1ace5f67cd26b6c2052
arodriguez <arodriguez@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-10 13:52:57 +0000

    Add localization support
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23265 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ src/org/pentaho/pac/client/i18n/PacLocalizedConstants.java
@@ -0,0 +1,10 @@
+package org.pentaho.pac.client.i18n;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface PacLocalizedConstants extends Constants {
+  String usersAndRoles();
+  String dataSources();
+  String services();
+  String scheduler();
+}
