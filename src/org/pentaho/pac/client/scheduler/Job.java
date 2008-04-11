1928d4176fd22dadf43706e14a5e5ea270c9d7e0
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-11 18:13:46 +0000

    header comment
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23407 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/scheduler/Job.java
+++ src/org/pentaho/pac/client/scheduler/Job.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2005-2008 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * Created  
+ * @author Steven Barkdull
+ */
 package org.pentaho.pac.client.scheduler;
 
 import java.io.Serializable;
 
 /**
- * NOTE: I wanted this class to be a simple POJO, not getters/setters, just
+ * NOTE: I wanted this class to be a simple POJO, no getters/setters, just
  * public member data. In GWT 1.4, the compiler seems to choke when there are 
  * no setters/getters. When the getters/setters are added, GWT is happy. Based
  * on MD's experience, this requirement does NOT exist in GWT 1.5.
