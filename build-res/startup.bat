29974fb752862d80f5a7cd50db1c10f6c094d3a1
arodriguez <arodriguez@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-14 19:38:26 +0000

    Fix start and stop scripts
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23566 5d404267-db13-0410-8476-b1f6b398e63a

--- build-res/startup.bat
+++ build-res/startup.bat
@@ -1,26 +1,3 @@
 @echo off
 
-REM ***************************************
-REM   BATCH SCRIPT TO START MGMG CONSOLE
-REM ***************************************
-
-if ""%1"" == ""un"" (set UN=%2)
-if ""%1"" == ""pwd"" (SET PWD=%2)
-if ""%3"" == ""un"" (set UN=%4)
-if ""%3"" == ""pwd"" (SET PWD=%4)
-
-if "%PWD%" == "" goto ret
-if "%UN%" == "" goto ret
-
-SET JAVA_PROPS= -Dpentaho.platform.username=%UN% -Dpentaho.platform.password=%PWD%
-
-
-call java -jar %JAVA_PROPS% C:\EclipseWorkspace\pentaho-administration-console\dist\pentaho-admin-console\pentaho-admin-console\pentaho-adminstration-console.jar
-
-goto end
-
-
-:ret
-echo "Invalid arguments! Usage: startup un=<USER_NAME> pwd=<PASSWORD>"
-
-:end
+call java -jar lib/pentaho-admin-console-1.8.0.jar
--- build-res/stop.bat
+++ build-res/stop.bat
@@ -5,4 +5,4 @@ REM   BATCH SCRIPT TO STOP MGMG CONSOLE
 REM ***************************************
 
 
-call java -jar C:\EclipseWorkspace\pentaho-administration-console\dist\pentaho-admin-console\pentaho-admin-console\pentaho-adminstration-console.jar STOP
+call java -jar lib/pentaho-adminstration-console.jar STOP
