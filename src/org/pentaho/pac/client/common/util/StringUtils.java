ce60ba56e47ffa8ec7d8dc661de6cf5ca1ba642c
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-04-16 14:18:48 +0000

    replicate functionality in apache stringutils
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@23646 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ src/org/pentaho/pac/client/common/util/StringUtils.java
@@ -0,0 +1,25 @@
+package org.pentaho.pac.client.common.util;
+
+// see: http://commons.apache.org/lang/apidocs/org/apache/commons/lang/StringUtils.html
+
+public class StringUtils {
+
+  public static boolean isEmpty( String str ) {
+    return null == str || "".equals( str );
+  }
+  
+  public static String defaultString( String str, String xdefault )
+  {
+    return StringUtils.isEmpty( str ) ? xdefault : str;
+  }
+  
+  public static String defaultString( String str )
+  {
+    return StringUtils.isEmpty( str ) ? "" : str; //$NON-NLS-1$
+  }
+  
+  public static String defaultIfEmpty( String str, String xdefault )
+  {
+    return StringUtils.isEmpty( str ) ? xdefault : str;
+  }
+}
