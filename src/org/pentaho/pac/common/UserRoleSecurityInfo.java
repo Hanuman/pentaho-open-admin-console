a4a7a5eace994da21f5b5ab9075a5358c647fa5a
arodriguez <arodriguez@5d404267-db13-0410-8476-b1f6b398e63a>
2008-05-14 14:56:52 +0000

    upgrade to gwt 1.5
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@25290 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/common/UserRoleSecurityInfo.java
+++ src/org/pentaho/pac/common/UserRoleSecurityInfo.java
@@ -4,36 +4,30 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.pentaho.pac.common.roles.ProxyPentahoRole;
+import org.pentaho.pac.common.users.ProxyPentahoUser;
+
 public class UserRoleSecurityInfo implements Serializable {
   
-  /**
-   * @gwt.typeArgs <org.pentaho.pac.common.users.ProxyPentahoUser>
-   */
-  ArrayList users = new ArrayList();
+  ArrayList<ProxyPentahoUser> users = new ArrayList<ProxyPentahoUser>();
   
-  /**
-   * @gwt.typeArgs <org.pentaho.pac.common.roles.ProxyPentahoRole>
-   */
-  ArrayList roles = new ArrayList();
+  ArrayList<ProxyPentahoRole> roles = new ArrayList<ProxyPentahoRole>();
   
-  /**
-   * @gwt.typeArgs <org.pentaho.pac.common.UserToRoleAssignment>
-   */
-  ArrayList assignments = new ArrayList();
+  ArrayList<UserToRoleAssignment> assignments = new ArrayList<UserToRoleAssignment>();
   
   public UserRoleSecurityInfo() {
     
   }
   
-  public List getUsers() {
+  public List<ProxyPentahoUser> getUsers() {
     return users;
   }
   
-  public List getRoles() {
+  public List<ProxyPentahoRole> getRoles() {
     return roles;
   }
   
-  public List getAssignments() {
+  public List<UserToRoleAssignment> getAssignments() {
     return assignments;
   }
 }
