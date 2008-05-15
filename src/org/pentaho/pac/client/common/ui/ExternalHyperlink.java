fee5f922334bb4dd3c327c82aa7665d77017f0e3
nbaker <nbaker@5d404267-db13-0410-8476-b1f6b398e63a>
2008-05-15 15:28:14 +0000

    External Hyperlink for GWT
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@25354 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ src/org/pentaho/pac/client/common/ui/ExternalHyperlink.java
@@ -0,0 +1,18 @@
+package org.pentaho.pac.client.common.ui;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ExternalHyperlink extends Widget{
+  public ExternalHyperlink(String label, String link){
+    super();
+     
+    Element a = DOM.createAnchor();
+    a.setAttribute("href", link);
+    a.setInnerHTML(label);
+    this.setElement(a);
+  }
+}
+
+   
