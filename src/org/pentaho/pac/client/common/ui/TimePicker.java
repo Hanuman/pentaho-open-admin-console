7bd1de5fd2d54b834d966535b0cec08feaed36f8
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-05-23 12:44:50 +0000

    provide getters and setters
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@25756 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/common/ui/TimePicker.java
+++ src/org/pentaho/pac/client/common/ui/TimePicker.java
@@ -1,19 +1,16 @@
 package org.pentaho.pac.client.common.ui;
 
-import java.util.Date;
+import org.pentaho.pac.client.common.util.TimeUtil;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 
+// TODO sbarkdull, this code needs to be locale sensitive
+
 public class TimePicker extends HorizontalPanel {
   private ListBox hourLB = new ListBox();
   private ListBox minuteLB = new ListBox();
-  private ListBox amPmLB = new ListBox();
-  
-  public static final int MAX_HOUR = 12;
-  public static final int MAX_MINUTE = 60;
-  public static final String AM = "AM";
-  public static final String PM = "PM";
+  private ListBox timeOfDayLB = new ListBox();
   
   public TimePicker() {
     
@@ -31,39 +28,65 @@ public class TimePicker extends HorizontalPanel {
 
     add( hourLB );
     add( minuteLB );
-    add( amPmLB );
+    add( timeOfDayLB );
   }
   
   /**
-   * format of string should be: HH:MM:AM/PM
+   * format of string should be: HH:MM:SS AM/PM
    * @param time
    */
   public void setTime( String time ) {
-    
+    throw new RuntimeException( "not implemented" );
   }
   
   public String getTime() {
-    return "";
+    throw new RuntimeException( "not implemented" );
   }
   private void initHourLB() {
     hourLB.setVisibleItemCount( 1 );
-    for ( int ii=1; ii<=MAX_HOUR; ++ii ) {
+    for ( int ii=1; ii<=TimeUtil.MAX_HOUR; ++ii ) {
       String strHr = Integer.toString( ii );
+      strHr = ( strHr.length() == 1 ) ? "0" + strHr : strHr;  // left pad single digit values with 0
       hourLB.addItem( strHr );
     }
   }
   
   private void initMinuteLB() {
     minuteLB.setVisibleItemCount( 1 );
-    for ( int ii=0; ii<MAX_MINUTE; ++ii ) {
+    for ( int ii=0; ii<TimeUtil.MAX_MINUTE; ++ii ) {
       String strMinute = Integer.toString( ii );
+      strMinute = ( strMinute.length() == 1 ) ? "0" + strMinute : strMinute;  // left pad single digit values with 0
       minuteLB.addItem( strMinute );
     }
   }
   
   private void initAmPmLB() {
-    amPmLB.setVisibleItemCount( 1 );
-    amPmLB.addItem( AM );
-    amPmLB.addItem( PM );
+    timeOfDayLB.setVisibleItemCount( 1 );
+    timeOfDayLB.addItem( TimeUtil.TimeOfDay.AM.toString() );
+    timeOfDayLB.addItem( TimeUtil.TimeOfDay.PM.toString() );
+  }
+
+  public String getHour() {
+    return hourLB.getItemText( hourLB.getSelectedIndex() );
+  }
+
+  public void setHour(String hour) {
+    this.hourLB.setSelectedIndex( Integer.parseInt( hour ) );
+  }
+
+  public String getMinute() {
+    return minuteLB.getItemText( minuteLB.getSelectedIndex() );
+  }
+
+  public void setMinute(String minute) {
+    this.minuteLB.setSelectedIndex( Integer.parseInt( minute ) );
+  }
+
+  public TimeUtil.TimeOfDay getTimeOfDay() {
+    return TimeUtil.TimeOfDay.get( timeOfDayLB.getSelectedIndex() );
+  }
+
+  public void setTimeOfDay(TimeUtil.TimeOfDay timeOfDay) {
+    this.timeOfDayLB.setSelectedIndex( timeOfDay.value() );
   }
 }
