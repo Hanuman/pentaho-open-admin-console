1e68e39eef7ab24603dd305d391e3cb064256778
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-05-30 20:37:33 +0000

    complete get/setTime() other work
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@26112 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/common/ui/TimePicker.java
+++ src/org/pentaho/pac/client/common/ui/TimePicker.java
@@ -1,12 +1,19 @@
 package org.pentaho.pac.client.common.ui;
 
+import org.pentaho.pac.client.common.EnumException;
 import org.pentaho.pac.client.common.util.TimeUtil;
+import org.pentaho.pac.client.common.util.TimeUtil.TimeOfDay;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 
 // TODO sbarkdull, this code needs to be locale sensitive
 
+/**
+ * Although hours combo displays the range 1...12, the hour 
+ * setter/getters take/return the range 0...11. So setting the hour to 0
+ * will cause 1 to be selected in the combo.
+ */
 public class TimePicker extends HorizontalPanel {
   private ListBox hourLB = new ListBox();
   private ListBox minuteLB = new ListBox();
@@ -32,22 +39,47 @@ public class TimePicker extends HorizontalPanel {
   }
   
   /**
-   * format of string should be: HH:MM:SS AM/PM
+   * format of string should be: HH:MM:SS AM/PM, e.g. 7:12:28 PM
    * @param time
    */
   public void setTime( String time ) {
-    throw new RuntimeException( "not implemented" );
+    String[] parts = time.split( ":" );
+    String[] lastParts = parts[2].split( "\\s" );
+    setHour( parts[0] );
+    setMinute( parts[1] );
+    //setSeconds( lastParts[0] );
+    TimeOfDay td;
+    try {
+      td = TimeOfDay.stringToTimeOfDay( lastParts[1] );
+    } catch (EnumException e) {
+      // TODO sbarkdull, bad fix this, rethrow something different?
+      e.printStackTrace();
+      return;
+    }
+    setTimeOfDay( td );
   }
   
+  /**
+   * format of string should be: HH:MM:SS AM/PM, e.g. 7:12:28 PM
+   * @return
+   */
   public String getTime() {
-    throw new RuntimeException( "not implemented" );
+    StringBuilder sb = new StringBuilder( getHour() )
+      .append( ":" )
+      .append( getMinute() )
+      .append( ":" )
+      .append( "00 " )
+      .append( getTimeOfDay().toString() );
+    
+    return sb.toString();
   }
   private void initHourLB() {
     hourLB.setVisibleItemCount( 1 );
     for ( int ii=1; ii<=TimeUtil.MAX_HOUR; ++ii ) {
-      String strHr = Integer.toString( ii );
-      strHr = ( strHr.length() == 1 ) ? "0" + strHr : strHr;  // left pad single digit values with 0
-      hourLB.addItem( strHr );
+      String strHrDisplay = Integer.toString( ii );
+      strHrDisplay = ( strHrDisplay.length() == 1 ) ? "0" + strHrDisplay : strHrDisplay;  // left pad single digit values with 0
+      String strHrValue = (ii == 12) ? "0" : Integer.toString( ii );
+      hourLB.addItem( strHrDisplay, strHrValue );
     }
   }
   
@@ -66,18 +98,41 @@ public class TimePicker extends HorizontalPanel {
     timeOfDayLB.addItem( TimeUtil.TimeOfDay.PM.toString() );
   }
 
+  /**
+   * Get the value (not the text) of the current selected item
+   * 
+   * NOTE: hours are 0...11, in spite of the fact that the combo displays 1...12
+   * 0...11 maps to 12,1,...11
+   * @return
+   */
   public String getHour() {
-    return hourLB.getItemText( hourLB.getSelectedIndex() );
+    return hourLB.getValue( hourLB.getSelectedIndex() );
   }
 
+
+  /**
+   * Set the hour you want to see visible in the control (i.e. 1,2,3,..12)
+   * 
+   * NOTE: hours are 0...11, in spite of the fact that the combo displays 1...12
+   * 0...11 maps to 12,1,...11
+   * @return
+   */
   public void setHour(String hour) {
-    this.hourLB.setSelectedIndex( Integer.parseInt( hour ) );
+    this.hourLB.setSelectedIndex( Integer.parseInt( hour )-1 );
   }
 
+  /**
+   * Get the value (not the text) of the current selected item
+   * @return
+   */
   public String getMinute() {
-    return minuteLB.getItemText( minuteLB.getSelectedIndex() );
+    return minuteLB.getValue( minuteLB.getSelectedIndex() );
   }
 
+  /**
+   * Set the minute you want to see visible in the control
+   * @param minute
+   */
   public void setMinute(String minute) {
     this.minuteLB.setSelectedIndex( Integer.parseInt( minute ) );
   }
