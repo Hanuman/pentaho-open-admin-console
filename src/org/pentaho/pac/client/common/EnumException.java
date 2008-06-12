af15688748b8ce0c18685e10291d8413a4fc85e9
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-06-12 16:10:02 +0000

    extend RuntimeException, since these EnumExceptions are due to programmer error, and the app can't recover from them.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@26849 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/common/EnumException.java
+++ src/org/pentaho/pac/client/common/EnumException.java
@@ -1,6 +1,6 @@
 package org.pentaho.pac.client.common;
 
-public class EnumException extends Exception {
+public class EnumException extends RuntimeException {
 
   public EnumException( String msg ) {
     super( msg );
