5f25674717b7a414c423f840e24fdedad69356c3
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-06-17 15:59:27 +0000

    add ErrorLabels
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@27152 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/scheduler/SolutionRepositoryItemPicker.java
+++ src/org/pentaho/pac/client/scheduler/SolutionRepositoryItemPicker.java
@@ -1,5 +1,7 @@
 package org.pentaho.pac.client.scheduler;
 
+import org.pentaho.pac.client.common.ui.widget.ErrorLabel;
+
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
@@ -10,31 +12,33 @@ public class SolutionRepositoryItemPicker extends FlexTable {
   private TextBox pathTb = new TextBox();
   private TextBox actionTb = new TextBox();
 
+  private ErrorLabel solutionLabel = null;
+  private ErrorLabel pathLabel = null;
+  private ErrorLabel actionLabel = null;
+
   public SolutionRepositoryItemPicker() {
     super();
 
     setCellPadding( 0 );
     setCellSpacing( 0 );
     
-    Label l = new Label( "Solution:" );
-    setWidget( 0 , 0, l );
+    solutionLabel = new ErrorLabel( new Label( "Solution:" ) );
+    setWidget( 0 , 0, solutionLabel );
     setWidget( 0 , 1, solutionTb );
-    
-    l = new Label( "Path:" );
-    setWidget( 1 , 0, l );
-    setWidget( 1 , 1, pathTb );
-    
-    l = new Label( "Action:" );
-    setWidget( 2 , 0, l );
-    setWidget( 2 , 1, actionTb );
 
+    pathLabel = new ErrorLabel( new Label( "Path:" ) );
+    setWidget( 1 , 0, pathLabel );
+    setWidget( 1 , 1, pathTb );
+
+    actionLabel = new ErrorLabel( new Label( "Action:" ) );
+    setWidget( 2 , 0, actionLabel );
+    setWidget( 2 , 1, actionTb );
   }
   
   public void reset() {    
-    // TODO sbarkdull tmp
-    solutionTb.setText( "" );
-    pathTb.setText( "" );
-    actionTb.setText( "" );
+    solutionTb.setText( "" ); //$NON-NLS-1$
+    pathTb.setText( "" ); //$NON-NLS-1$
+    actionTb.setText( "" ); //$NON-NLS-1$
   }
   
   public String getSolution() {
@@ -59,6 +63,18 @@ public class SolutionRepositoryItemPicker extends FlexTable {
   
   public void setAction( String action ) {
     actionTb.setText( action );
+  }
+  
+  public void setSolutionError( String errorMsg ) {
+    solutionLabel.setErrorMsg( errorMsg );
+  }
+  
+  public void setPathError( String errorMsg ) {
+    pathLabel.setErrorMsg( errorMsg );
+  }
+  
+  public void setActionError( String errorMsg ) {
+    actionLabel.setErrorMsg( errorMsg );
   }
   
 }
