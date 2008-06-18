c6d25fecf6848cfe78fbc036721e0eef6568b887
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-06-18 17:07:46 +0000

    move a bunch of is* methods from CronParser to here.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@27221 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/common/util/StringUtils.java
+++ src/org/pentaho/pac/client/common/util/StringUtils.java
@@ -34,4 +34,27 @@ public class StringUtils {
   public static String divideStringWithInt( String strDividend, int divisor ) {
     return Integer.toString( Integer.parseInt( strDividend ) / divisor );
   }
+
+  // MAX_INT = 2147483647
+  private static final String MATCH_POSITIVE_INTEGER_RE = "^\\s*[0-9]{1,10}\\s*$"; //$NON-NLS-1$
+  public static boolean isPositiveInteger( String strInt ) {
+    int length = strInt.length();
+    if ( length > 10 ) {
+      return false; // MAX_INT has 10 digits
+    } else if ( length == 10 ) {
+      // This is an execution-expensive path, since it relies on exception
+      // processing to determine if strInt is an Integer or not. Minimize
+      // the number of times this else-if is entered
+      if ( strInt.matches( MATCH_POSITIVE_INTEGER_RE ) ) {
+        try {
+          Integer.parseInt( strInt );
+          return true;
+        } catch( NumberFormatException ignore ) {
+        }
+      }
+      return false;
+    } else {
+      return strInt.matches( MATCH_POSITIVE_INTEGER_RE );
+    }
+  }
 }
