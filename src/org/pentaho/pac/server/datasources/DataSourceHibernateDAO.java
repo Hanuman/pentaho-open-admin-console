68e17479a2f03f8876872afab7171dbf5fc5de55
rmansoor <rmansoor@5d404267-db13-0410-8476-b1f6b398e63a>
2008-07-15 17:52:29 +0000

    Making the code changes due to the new gwt jar
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@29086 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/server/datasources/DataSourceHibernateDAO.java
+++ src/org/pentaho/pac/server/datasources/DataSourceHibernateDAO.java
@@ -7,14 +7,10 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.exception.JDBCConnectionException;
 import org.pentaho.pac.common.datasources.DuplicateDataSourceException;
-import org.pentaho.pac.common.datasources.IPentahoDataSource;
 import org.pentaho.pac.common.datasources.NonExistingDataSourceException;
 import org.pentaho.pac.common.datasources.PentahoDataSource;
 import org.pentaho.pac.server.common.DAOException;
 import org.pentaho.pac.server.common.HibernateSessionFactory;
-import org.pentaho.platform.api.util.IPasswordService;
-import org.pentaho.platform.api.util.PasswordServiceException;
-import org.pentaho.platform.util.Base64PasswordService;
 
 public class DataSourceHibernateDAO implements IDataSourceDAO {
 
@@ -24,14 +20,7 @@ public class DataSourceHibernateDAO implements IDataSourceDAO {
   public void createDataSource(PentahoDataSource newDataSource) throws DuplicateDataSourceException, DAOException {
     if (getDataSource(newDataSource.getName()) == null) {
       try {
-    	// Get the password service
-        IPasswordService passwordService = new Base64PasswordService();
-        // Store the new encrypted password in the datasource object
-        newDataSource.setPassword(passwordService.encrypt(newDataSource.getPassword()));
         getSession().save(newDataSource);
-      } catch(PasswordServiceException pse) {
-     	getSession().evict(newDataSource);
-        throw new DAOException( pse.getMessage(), pse );
       } catch (HibernateException ex) {
         getSession().evict(newDataSource);
         throw new DAOException( ex.getMessage(), ex );
@@ -57,13 +46,7 @@ public class DataSourceHibernateDAO implements IDataSourceDAO {
   public PentahoDataSource getDataSource(String jndiName) throws DAOException {
     try {
       PentahoDataSource pentahoDataSource = (PentahoDataSource) getSession().get(PentahoDataSource.class.getName(), jndiName);
-      if(pentahoDataSource != null) {
-        IPasswordService passwordService = new Base64PasswordService();
-        pentahoDataSource.setPassword(passwordService.decrypt(pentahoDataSource.getPassword()));
-      }
       return pentahoDataSource;
-    } catch(PasswordServiceException pse) {
-      throw new DAOException( pse.getMessage(), pse );
     } catch (HibernateException ex) {
       throw new DAOException(ex.getMessage(), ex);
     }
@@ -74,17 +57,6 @@ public class DataSourceHibernateDAO implements IDataSourceDAO {
       String queryString = "from PentahoDataSource";  //$NON-NLS-1$
       Query queryObject = getSession().createQuery(queryString);
       List<PentahoDataSource> pentahoDataSourceList = queryObject.list();
-      for(PentahoDataSource pentahoDataSource: pentahoDataSourceList) {
-		  try {
-	   	      // Get the password service
-			  if(pentahoDataSource != null) {
-	        IPasswordService passwordService = new Base64PasswordService();
-	        pentahoDataSource.setPassword(passwordService.decrypt(pentahoDataSource.getPassword()));
-			  }
-		  } catch(PasswordServiceException pse) {
-		    throw new DAOException( pse.getMessage(), pse );
-		  }     	  
-      }
       return pentahoDataSourceList;
     } catch (HibernateException ex) {
       throw new DAOException( ex.getMessage(), ex );
@@ -94,13 +66,7 @@ public class DataSourceHibernateDAO implements IDataSourceDAO {
   public void updateDataSource(PentahoDataSource dataSource) throws NonExistingDataSourceException, DAOException {
     if (getDataSource(dataSource.getName()) != null) {
       try {
-      	// Get the password service
-        IPasswordService passwordService = new Base64PasswordService();
-        // Store the new encrypted password in the datasource object
-        dataSource.setPassword(passwordService.encrypt(dataSource.getPassword()));
         getSession().update(dataSource);
-      } catch(PasswordServiceException pse) {
-          throw new DAOException( pse.getMessage(), pse );
       } catch (HibernateException ex) {
         throw new DAOException( ex.getMessage(), ex );
       }
