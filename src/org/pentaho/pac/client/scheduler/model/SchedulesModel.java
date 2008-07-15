976a27708068ec3f1470c602fa5e3a988feb8cfb
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-07-15 20:15:34 +0000

    change DoubleKeyMap implementation to simple HashMap implementation.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@29096 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/scheduler/model/SchedulesModel.java
+++ src/org/pentaho/pac/client/scheduler/model/SchedulesModel.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2005-2008 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * Created  
+ * @author Steven Barkdull
+ */
 package org.pentaho.pac.client.scheduler.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.pentaho.pac.client.common.util.DoubleKeyMap;
+import java.util.Map;
 
 /**
  * 
@@ -11,37 +27,38 @@ import org.pentaho.pac.client.common.util.DoubleKeyMap;
  */
 public class SchedulesModel {
 
-  private DoubleKeyMap<String, String, Schedule> scheduleMap = new DoubleKeyMap<String, String, Schedule>();
+  //private DoubleKeyMap<String, String, Schedule> scheduleMap = new DoubleKeyMap<String, String, Schedule>();
+  private Map<String, Schedule> scheduleMap = new HashMap<String, Schedule>();
   
   public SchedulesModel() {
   }
   
   public void add( List<Schedule> l ) {
     for ( Schedule schedule : l ) {
-      add( schedule.getJobName(), schedule.getJobGroup(), schedule );
+      add( schedule.getJobName(), schedule );
     }
   }
   
   // TODO sbarkdull, need to do something in case client tries to add some key-pair more than once, exception?
-  public void add( String name, String groupName, Schedule schedule ) {
-    scheduleMap.put( groupName, name, schedule );
+  public void add( String name, Schedule schedule ) {
+    scheduleMap.put( name, schedule );
   }
   
-  public void remove( String name, String groupName ) {
-    scheduleMap.remove( groupName, name );
+  public void remove( String name ) {
+    scheduleMap.remove( name );
   }
   
   public void remove( List<Schedule> l ) {
     for ( Schedule s : l ) {
-      remove( s.getJobName(), s.getJobGroup() );
+      remove( s.getJobName() );
     }
   }
   
-  public Schedule get( String name, String groupName ) {
-    return scheduleMap.get( groupName, name );
+  public Schedule get( String name ) {
+    return scheduleMap.get( name );
   }
   
   public List<Schedule> getScheduleList() {
-    return scheduleMap.getList();
+    return new ArrayList<Schedule>( scheduleMap.values() );
   }
 }
