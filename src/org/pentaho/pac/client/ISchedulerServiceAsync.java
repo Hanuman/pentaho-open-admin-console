78ac892f2b78ef49a5aa5563584570980afe37f3
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-07-22 16:48:40 +0000

    generics
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@29673 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/ISchedulerServiceAsync.java
+++ src/org/pentaho/pac/client/ISchedulerServiceAsync.java
@@ -14,25 +14,25 @@ public interface ISchedulerServiceAsync {
   public void getJobNames( AsyncCallback<Map<String,Schedule>> callback );
   public void createCronSchedule( String jobName, String jobGroup, String description,
       Date startDate, Date endDate,
-      String cronString, String actionsList, AsyncCallback callback );
+      String cronString, String actionsList, AsyncCallback<Object> callback );
   public void createRepeatSchedule( String jobName, String jobGroup, String description,
       Date startDate, Date endDate,
       String strRepeatCount, String repeatInterval,
-      String actionsList, AsyncCallback callback );
+      String actionsList, AsyncCallback<Object> callback );
 
   public void updateCronSchedule( String oldJobName, String oldJobGroup, String schedId,
       String jobName, String jobGroup, String description,
       Date startDate, Date endDate,
       String cronString, 
-      String actionsList, AsyncCallback callback );
+      String actionsList, AsyncCallback<Object> callback );
   public void updateRepeatSchedule( String oldJobName, String oldJobGroup, String schedId,
       String jobName, String jobGroup, String description,
       Date startDate, Date endDate,
       String strRepeatCount,String repeatInterval,
-      String actionsList, AsyncCallback callback );
+      String actionsList, AsyncCallback<Object> callback );
   
-  public void deleteJobs( List<Schedule> scheduleList, AsyncCallback callback );
-  public void pauseJobs( List<Schedule> scheduleList, AsyncCallback callback );
-  public void resumeJobs( List<Schedule> scheduleList, AsyncCallback callback );
-  public void executeJobs( List<Schedule> scheduleList, AsyncCallback callback );
+  public void deleteJobs( List<Schedule> scheduleList, AsyncCallback<Object> callback );
+  public void pauseJobs( List<Schedule> scheduleList, AsyncCallback<Object> callback );
+  public void resumeJobs( List<Schedule> scheduleList, AsyncCallback<Object> callback );
+  public void executeJobs( List<Schedule> scheduleList, AsyncCallback<Object> callback );
 }
