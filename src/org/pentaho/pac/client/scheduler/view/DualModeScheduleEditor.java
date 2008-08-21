ccf98fb244074dc7e27267c06be6d9b86931995f
sbarkdull <sbarkdull@5d404267-db13-0410-8476-b1f6b398e63a>
2008-08-21 21:18:53 +0000

    i18n
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@31137 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/scheduler/view/DualModeScheduleEditor.java
+++ src/org/pentaho/pac/client/scheduler/view/DualModeScheduleEditor.java
@@ -1,19 +1,29 @@
 package org.pentaho.pac.client.scheduler.view;
 
 import org.pentaho.gwt.widgets.client.controls.schededitor.ScheduleEditor;
+import org.pentaho.pac.client.PentahoAdminConsole;
+import org.pentaho.pac.client.i18n.PacLocalizedMessages;
 
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 
+/**
+ * "Dual mode" refers to the fact that this editor can handle both
+ * subscription and regular schedules (public and private)
+ * 
+ * @author Steven Barkdull
+ *
+ */
 public class DualModeScheduleEditor extends ScheduleEditor {
 
   private CheckBox subscriptionCb = new CheckBox();
+  private static final PacLocalizedMessages MSGS = PentahoAdminConsole.getLocalizedMessages();
   
   public DualModeScheduleEditor() {
     super();
     HorizontalPanel p = new HorizontalPanel();
-    Label l = new Label( "Subscription Schedule" );
+    Label l = new Label( MSGS.scheduleTypeCheckboxLabel() );
     p.add( subscriptionCb );
     p.add( l );
     this.insert( p, 0 );
