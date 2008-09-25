202b4d34e023fee539fffaa727f5f4ec7f22fd3d
wgorman <wgorman@5d404267-db13-0410-8476-b1f6b398e63a>
2008-09-25 20:06:38 +0000

    [BUILD] fixed scripts to dynamically load classpath for jdbc driver jars
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@32724 5d404267-db13-0410-8476-b1f6b398e63a

--- build-res/start.sh
+++ build-res/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "JAVA_HOME set to $JAVA_HOME"
-
 S1="x$JAVA"
 S2="x"
 if [ $S1=$S2 ]; then
@@ -13,5 +11,13 @@ if [ $S1=$S2 ]; then
   fi
 fi
 
-echo "JAVA is $JAVA"
+CLASSPATH=resource/config
+
+files=$(ls ./lib/*.jar)
+
+for i in $files
+do
+  CLASSPATH="$CLASSPATH:$i"
+done
+
 $JAVA -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M  -DCONSOLE_HOME=. -Dlog4j.configuration=resource/config/log4j.xml -jar lib/pentaho-open-admin-console.jar
--- build-res/startup.bat
+++ build-res/startup.bat
@@ -1,2 +1,17 @@
 @echo off
-call java -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -DCONSOLE_HOME=. -Dlog4j.configuration=resource/config/log4j.xml -jar lib/pentaho-open-admin-console.jar
+REM ***************************************
+REM   BATCH SCRIPT TO START ADMIN CONSOLE
+REM ***************************************
+set CLASSPATH=resource\config
+
+FOR %%F IN (lib\*.jar) DO call :updateClassPath %%F
+FOR %%F IN (jdbc\*.jar) DO call :updateClassPath %%F
+
+goto :startjava
+
+:updateClassPath
+set CLASSPATH=%CLASSPATH%;%1
+goto :eof
+
+:startjava
+call java -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -DCONSOLE_HOME=. -Dlog4j.configuration=resource/config/log4j.xml -cp %CLASSPATH%  org.pentaho.pac.server.JettyServer
