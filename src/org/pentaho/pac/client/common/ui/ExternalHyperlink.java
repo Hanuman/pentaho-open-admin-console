30ba9db9ce7e2d0591144a91d060aabd373ec0c2
bholecko <bholecko@5d404267-db13-0410-8476-b1f6b398e63a>
2008-10-29 13:59:18 +0000

    added ability to open link in new window
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@34105 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/pac/client/common/ui/ExternalHyperlink.java
+++ src/org/pentaho/pac/client/common/ui/ExternalHyperlink.java
@@ -5,14 +5,26 @@ import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ExternalHyperlink extends Widget{
+  
   public ExternalHyperlink(String label, String link){
+    this(label, link, false);
+  }
+  
+  public ExternalHyperlink(String label, String link, boolean isOpenInNewWindow){
     super();
      
     Element a = DOM.createAnchor();
+    
     a.setAttribute("href", link); //$NON-NLS-1$
+    
+    if (isOpenInNewWindow){
+      a.setAttribute("target", "_blank"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+    
     a.setInnerHTML(label);
+    
     this.setElement(a);
   }
+  
 }
-
    
