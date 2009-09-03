aa53605e818d6ee4087e9f39221393f840debb21
mlowery <mlowery@5d404267-db13-0410-8476-b1f6b398e63a>
2009-09-03 16:27:07 +0000

    [PPP-2125] [PPP-2055]
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-open-admin-console/trunk@39366 5d404267-db13-0410-8476-b1f6b398e63a

--- build-res/start-pac.bat
+++ build-res/start-pac.bat
@@ -13,4 +13,22 @@ set CLASSPATH=%CLASSPATH%;%1
 goto :eof
 
 :startjava
-call java -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -DCONSOLE_HOME=. -Dlog4j.configuration=resource/config/log4j.xml -cp %CLASSPATH%  org.pentaho.pac.server.JettyServer
+
+if defined JAVA_HOME goto use_java_home
+if defined JRE_HOME goto use_jre_home
+pushd ..\biserver-ce
+set PENTAHO_PATH=%CD%\
+popd
+set PENTAHO_JAVA="%PENTAHO_PATH%jre\bin\java"
+goto pac
+
+:use_java_home
+set PENTAHO_JAVA="%JAVA_HOME%\bin\java"
+goto pac
+
+:use_jre_home
+set PENTAHO_JAVA="%JRE_HOME%\bin\java"
+goto pac
+
+:pac
+call %PENTAHO_JAVA% -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -DCONSOLE_HOME=. -Dlog4j.configuration=resource/config/log4j.xml -cp %CLASSPATH%  org.pentaho.pac.server.JettyServer
--- build-res/stop-pac.bat
+++ build-res/stop-pac.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM ***************************************
-REM   BATCH SCRIPT TO STOP MGMG CONSOLE
+REM   BATCH SCRIPT TO STOP ADMIN CONSOLE
 REM ***************************************
 set PATH=%path%
 set CLASSPATH=.;./resource;./bin;./classes;./lib;
@@ -16,4 +16,22 @@ goto :eof
 
 :startjava
 echo %CLASSPATH%
+
+if defined JAVA_HOME goto use_java_home
+if defined JRE_HOME goto use_jre_home
+pushd ..\biserver-ce
+set PENTAHO_PATH=%CD%\
+popd
+set PENTAHO_JAVA="%PENTAHO_PATH%jre\bin\java"
+goto pac
+
+:use_java_home
+set PENTAHO_JAVA="%JAVA_HOME%\bin\java"
+goto pac
+
+:use_jre_home
+set PENTAHO_JAVA="%JRE_HOME%\bin\java"
+goto pac
+
+:pac
 java -Djava.io.tmpdir=temp -cp %CLASSPATH% org.pentaho.pac.server.StopJettyServer
